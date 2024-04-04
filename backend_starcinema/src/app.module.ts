025edc695be3df68d154e5df7fc7eb819380bf4c
GeorgeU2030 <georgeu2021@gmail.com>
2024-04-04 10:09:21 -0500

    chore: setup_modules_for_the_project

--- backend_starcinema/src/app.module.ts
+++ backend_starcinema/src/app.module.ts
@@ -5,9 +5,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TicketsModule } from './tickets/tickets.module';
+import { MoviesModule } from './movies/movies.module';
+import { FunctionsModule } from './functions/functions.module';
+import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UsersModule, TicketsModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule, TicketsModule, MoviesModule, FunctionsModule, RoomsModule],
   controllers: [AppController],
   providers: [AppService],
 })
