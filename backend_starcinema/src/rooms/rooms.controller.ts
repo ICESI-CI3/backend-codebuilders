758e4cbcba5ccccb17d6a4c2002c16c561c39115
Ricardo Medina Sterling <ricardoms1909@gmail.com>
2024-04-06 23:01:44 -0500

    feat: add_admin_role_restriction_in_rooms_crud

--- backend_starcinema/src/rooms/rooms.controller.ts
+++ backend_starcinema/src/rooms/rooms.controller.ts
@@ -12,6 +12,8 @@ import {
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
 import { RoomsService } from './rooms.service';
+import { Roles } from 'src/roles/roles.decorator';
+import { Role } from 'src/roles/roles.enum';
 
 @Controller('rooms')
 export class RoomsController {
@@ -35,18 +37,21 @@ export class RoomsController {
 		return this.roomsService.findOneById(id);
 	}
 
+	@Roles(Role.Admin)
 	@HttpCode(HttpStatus.CREATED)
 	@Post('create')
 	create(@Body() room: CreateRoomDto) {
 		return this.roomsService.create(room);
 	}
 
+	@Roles(Role.Admin)
 	@HttpCode(HttpStatus.OK)
 	@Put(':id')
 	update(@Body() room: UpdateRoomDto, @Param('id') id: number) {
 		return this.roomsService.update(id, room);
 	}
 
+	@Roles(Role.Admin)
 	@HttpCode(HttpStatus.OK)
 	@Delete(':id')
 	delete(@Param('id') id: number) {
